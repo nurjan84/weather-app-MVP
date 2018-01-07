d1f6a247d443c6967fb2f25908bf9edd45260536
nurzhan84 <nurzhantolegenov@gmail.com>
2018-01-07 16:24:13 +0600

    AddToEndSingleStrategy

--- app/src/main/java/kz/nta/kazweather/mvp/views/interfaces/MainActivityView.kt
+++ app/src/main/java/kz/nta/kazweather/mvp/views/interfaces/MainActivityView.kt
@@ -1,6 +1,7 @@
 package kz.nta.kazweather.mvp.views.interfaces
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
 import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import kz.nta.kazweather.mvp.models.WeatherModel
@@ -9,7 +10,7 @@ import kz.nta.kazweather.mvp.models.WeatherModel
 interface MainActivityView:MvpView{
     fun onShowLoader()
     fun onHideLoader()
-
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun onWeatherLoaded(resultList : ArrayList<WeatherModel>)
     fun onErrorWeatherLoading(error:Throwable?)
 }
